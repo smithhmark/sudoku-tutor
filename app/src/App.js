f76f281d99caec23451192ce966ab00ee40e587f
Mark Smith <smith.h.mark@gmail.com>
2018-02-23 15:13:18 -0800

    adding Puzzle obj

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Square from './Puzzle.js'
+import Puzzle from './Puzzle.js'
+
+let examplePuzzle = [
+        undefined,undefined,undefined, 2,6,undefined, 7,undefined,1,
+        6,8,undefined, undefined,7,undefined, undefined,9,undefined,
+        1,9,undefined, undefined,undefined,4, 5,undefined,undefined,
+
+        8,2,undefined, 1,undefined,undefined, undefined,4,undefined,
+        undefined,undefined,4, 6,undefined,2, 9,undefined,undefined,
+        undefined,5,undefined, undefined,undefined,3, undefined,2,8,
+
+        undefined,undefined,9, 3,undefined,undefined, undefined,7,4,
+        undefined,4,undefined, undefined,5,undefined, undefined,3,6,
+        7,undefined,3, undefined,1,8, undefined,undefined,undefined,]
 
 class App extends Component {
   render() {
@@ -14,8 +27,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      <Square symbols={[1,2,3,4,]} value={1}>
-      </Square>
+      <Puzzle puzzle={examplePuzzle} />
       </div>
     );
   }
--- app/src/Puzzle.js
+++ app/src/Puzzle.js
@@ -1,27 +1,39 @@
 import React, { Component } from 'react';
 
-//function Square(props) {
 class Square extends Component {
   constructor(props) {
     super(props);
-    this.symbols = props.symbols;
-    this.state = {"value": props.value}
+    //console.log("Square props:", props);
+    this.state = {
+      value: props.value,
+      symbols: props.symbols,
+    };
+    this.deltaCB = props.cb;
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(event) {
-    console.log("handling change", event.target);
-    this.setState({value: event.target.value});
+    console.log("Square handling change", event.target);
+    const st = this.state;
+    this.setState({
+      value: event.target.value,
+      symbols: st.symbols,
+    });
+    this.deltaCB(event.target.value);
   }
 
   render() {
     let opts = []
-    for (let ii = 0; ii < this.symbols.length ; ii++) {
+    opts.push(
+        <option key={0} value={0}></option>);
+    for (let ii = 1; ii < this.state.symbols.length ; ii++) {
+      let val = this.state.symbols[ii]
       opts.push(
-        <option key={ii} value={this.symbols[ii]}>{this.symbols[ii]}</option>);
+        <option key={ii} value={val}>{val}</option>);
     }
+    const val = this.state.value;
     return <select 
-      className="square" value={this.state.value} onChange={this.handleChange}>
+      className="square" value={val} onChange={this.handleChange}>
         {opts}
       </select>;
   }
@@ -30,32 +42,68 @@ class Square extends Component {
 class Puzzle extends Component {
   constructor(props) {
     super(props);
+    //console.log("Puzzle props:", props);
     if (props.puzzle.length === 81) {
       this.symbolCnt = 9;
     } else if (props.puzzle.length === 16) {
       this.symbolCnt = 4;
     } else if (props.puzzle.length === 4) {
       this.symbolCnt = 2;
+    } else {
+      this.symbolCnt = 2;
     }
-    this.initial = props.puzzle;
     this.state = {
       initial: props.puzzle,
-      puz: props.puzzle.slice(),
+      puzzle: props.puzzle.slice(),
+      symbols: [],
     };
-    this.symbols = []
-    for (let ii = 0; ii < this.symbolCnt; ii++) {
-      this.symbols.push(ii)
+    for (let ii = 0; ii < this.symbolCnt+1; ii++) {
+      this.state.symbols.push(ii)
     }
   }
 
   renderSquare(idx) {
-    return <Square ></Square>
+    return (
+      <Square 
+        key={idx}
+        value={this.state.puzzle[idx]}
+        symbols={this.state.symbols}
+        cb={(v) => this.updateSquare(idx,v)}/>
+    );
+  }
+
+  renderRow(row) {
+    let data = [];
+    for ( let ii = 0; ii < this.state.symbols.length; ii++) {
+      let childidx = this.state.symbols.length * row + ii;
+      data.push(<td key={ii}>{this.renderSquare(childidx)}</td>);
+    }
+    return data;
+  }
+
+  updateSquare(idx, newval) {
+    console.log("Puzzle updating state");
+    //console.log("  old:", this.state.puzzle[idx]);
+    //console.log("  new:", newval);
+    const st = this.state;
+    let pz = st.puzzle.slice();
+    pz[idx] = newval;
+    this.setState({
+      puzzle: pz,
+      symbols: st.symbols,
+      initial: st.initial,
+    });
   }
 
   render() {
-    return <table></table>
+    let rows = [];
+    for ( let ii = 0; ii < this.state.symbols.length; ii++) {
+      rows.push(<tr key={ii}>{this.renderRow(ii)}</tr>);
+    }
+
+    return <table><tbody>{rows}</tbody></table>
   }
 
 }
 
-export default Square
+export default Puzzle
