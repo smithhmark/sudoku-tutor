546da6a7c7c83a1f9165799465a11673fa836a91
Mark Smith <smith.h.mark@gmail.com>
2018-02-23 15:52:20 -0800

    adding shading for boxes

--- app/src/Puzzle.js
+++ app/src/Puzzle.js
@@ -45,10 +45,33 @@ class Puzzle extends Component {
     //console.log("Puzzle props:", props);
     if (props.puzzle.length === 81) {
       this.symbolCnt = 9;
+      this.boxes = [
+        new Set([0,1,2,9,10,11,18,19,20, ]),
+        new Set([3,4,5,12,13,14,21,22,23, ]),
+        new Set([6,7,8,15,16,17,24,25,26 ]),
+        new Set([27,28,29,36,37,38,45,46,47, ]),
+        new Set([30,31,32,39,40,41,48,49,50, ]),
+        new Set([33,34,35,42,43,44,51,52,53, ]),
+        new Set([54,55,56,63,64,65,72,73,74, ]),
+        new Set([57,58,59,66,67,68,75,76,77, ]),
+        new Set([60,61,62,69,70,71,78,79,80 ]),
+      ];
+      this.darkened = new Set([0, 2,4,6,8]);
     } else if (props.puzzle.length === 16) {
       this.symbolCnt = 4;
+      this.boxes = [
+        new Set([0,1,4,5, ]),
+        new Set([2,3,6,7, ]),
+        new Set([8,9, 12,13]),
+        new Set([10, 11, 14,15]),
+      ];
+      this.darkened = new Set([0, 3,]);
     } else if (props.puzzle.length === 4) {
       this.symbolCnt = 2;
+      this.boxes = [
+        [0],[1],[2],[3],
+      ];
+      this.darkened = new Set([0, 3,]);
     } else {
       this.symbolCnt = 2;
     }
@@ -62,21 +85,43 @@ class Puzzle extends Component {
     }
   }
 
-  renderSquare(idx) {
-    return (
-      <Square 
-        key={idx}
-        value={this.state.puzzle[idx]}
-        symbols={this.state.symbols}
-        cb={(v) => this.updateSquare(idx,v)}/>
-    );
+  renderSquare(row, col) {
+    let idx = (this.state.symbols.length-1) * row + col;
+    let dark = false;
+    for (let bx = 0; bx< this.boxes.length ; bx++) {
+      if (this.boxes[bx].has(idx)) {
+        if (this.darkened.has(bx)) {
+          dark = true;
+        }
+      }
+    }
+    if (dark) {
+      return (
+        <span 
+          key={idx}
+          style={{backgroundColor:'gray'}}>
+
+        <Square 
+          value={this.state.puzzle[idx]}
+          symbols={this.state.symbols}
+          cb={(v) => this.updateSquare(idx,v)}/>
+        </span>
+      );
+    } else {
+      return (
+        <Square 
+          key={idx}
+          value={this.state.puzzle[idx]}
+          symbols={this.state.symbols}
+          cb={(v) => this.updateSquare(idx,v)}/>
+      );
+    }
   }
 
   renderRow(row) {
     let data = [];
     for ( let ii = 0; ii < this.state.symbols.length - 1; ii++) {
-      let childidx = (this.state.symbols.length-1) * row + ii;
-      data.push(<td key={ii}>{this.renderSquare(childidx)}</td>);
+      data.push(<td key={ii}>{this.renderSquare(row, ii)}</td>);
     }
     return data;
   }
